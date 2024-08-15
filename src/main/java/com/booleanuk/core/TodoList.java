38abbd1395d68b2605621e64a21801cb0bfbd903
TMajlu <thomasmajnie@gmail.com>
2024-08-15 09:20:17 +0200

    Made add test.

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,5 +1,15 @@
 package com.booleanuk.core;
-
+import java.util.HashMap;
 public class TodoList {
+    HashMap<String, String> taskList=new HashMap<String, String>();
 
+    public boolean add(String task){
+        if(!taskList.containsKey(task)){
+            taskList.put(task, "Incomplete");
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -4,10 +4,20 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TodoListTest {
+
+    public TodoListTest(){}
     @Test
     public void exampleTest() {
         String hello = "Hello";
         Assertions.assertEquals("Hello", hello);
         Assertions.assertNotEquals("Goodbye", hello);
+    }
+
+    @Test
+    public void addTest() {
+        TodoList todoList=new TodoList();
+
+        Assertions.assertTrue(todoList.add("Write add() test"));
+        Assertions.assertFalse(todoList.add("Write add() test"));
     }
 }
