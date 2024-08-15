f538bde16bb7534f393e8cce93dd0737220085a8
TMajlu <thomasmajnie@gmail.com>
2024-08-15 11:12:44 +0200

    Altered the method by which to compare arrays.

--- src/test/java/com/booleanuk/core/TodoListTest.java
+++ src/test/java/com/booleanuk/core/TodoListTest.java
@@ -78,8 +78,8 @@ class TodoListTest {
         todoList1.add("C test");
         todoList1.add("A test");
         String[] testList0= new String[]{"A test", "B test", "C test"};
-        String[] testList1= new String[]{"A test", "B test", "C test"};
+        String[] testList1= new String[]{"C test", "B test", "A test"};
 
-        Assertions.assertTrue(Arrays.equals(testList0, testList1));
+        Assertions.assertTrue(Arrays.equals(testList0, todoList0.sort(true)));
     }
 }
